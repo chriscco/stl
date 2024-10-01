8f4d3cce79d8b44bd531853e8f06272fa7b53662
chriscco <2444628489c@gmail.com>
2024-09-30 21:01:59 -0700

    initially implemented

--- share_pointer/sharedPointer.cpp
+++ share_pointer/sharedPointer.cpp
@@ -1,22 +1,61 @@
 #include <iostream>
 
 template <class T>
+struct SpControlBlock {
+    T* my_ptr;
+    int ref_count; // 保存一共有多指针共享当前的地址
+    explicit SpControlBlock(T* ptr) : my_ptr(ptr), ref_count(1) {};
+
+    SpControlBlock(SpControlBlock&& that) = delete;
+
+    ~SpControlBlock() {
+        delete my_ptr;
+    }
+};
+
+template <class T>
 class SharedPointer {
 private :
-    T* my_ptr;
+    SpControlBlock<T>* control_b;
 public:
-    explicit SharedPointer(T *ptr) : my_ptr(ptr) {};
+    explicit SharedPointer(T *ptr) : control_b(new SpControlBlock(ptr)) {};
+    SharedPointer(SharedPointer const& that) : control_b(that.control_b) {
+        control_b->ref_count++;
+    }
+
+    ~SharedPointer() {
+        control_b->ref_count--;
+        if (control_b->ref_count == 0) {
+            delete control_b;
+        }
+    }
+
+    T* get() const { return control_b->my_ptr; }
+
+    T* operator->() const { return control_b->my_ptr; }
+
+    T& operator*() const { return *control_b->my_ptr; }
 };
+
+template<class T, class... Args>
+SharedPointer<T> makeShared(Args &&... args) {
+    return SharedPointer<T>(new T(std::forward<Args>(args)...));
+}
 
 class MyClass {
 public:
     int age;
-    explicit MyClass(int age) : age(age) {};
+    const char* name;
+    explicit MyClass(int age_, const char* name_) : age(age_), name(name_) {
+        std::cout << "construct" << std::endl;
+    };
+    ~MyClass() {
+        std::cout << "deconstruct" << std::endl;
+    }
 };
 
 int main() {
-    SharedPointer p0(new MyClass(10));
-    std::cout << &p0 << std::endl;
+    SharedPointer p0 = makeShared<MyClass>(20, "kaka");
     SharedPointer p1 = p0;
-    std::cout << &p1 << std::endl;
+    MyClass* raw = p0.get();
 }
