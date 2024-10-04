c5a23c07f20cfb62728d4d3c0bfeaf0dcb1b00e5
chriscco <2444628489c@gmail.com>
2024-10-03 21:25:21 -0700

    initial commit

---
+++ functional/function.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Chris Cao on 10/3/24.
+//
+
+#include "function.hpp"
---
+++ functional/function.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Chris Cao on 10/3/24.
+//
+
+#ifndef STL_FUNCTION_HPP
+#define STL_FUNCTION_HPP
+
+
+class function {
+
+};
+
+
+#endif //STL_FUNCTION_HPP
