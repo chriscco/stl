798a8e09611a258e36af2b58406b0426c0df834c
chriscco <2444628489c@gmail.com>
2024-10-04 17:05:50 -0700

    printVector()

--- vectors/vector_impl.cpp
+++ vectors/vector_impl.cpp
@@ -1,8 +1,31 @@
 #include "vector_impl.hpp"
 #include <iostream>
 
+void printVector(Vectors const& vec) {
+    std::cout << "&Vector: " << &vec << std::endl;
+    for (int i = 0; i < vec.size(); i++) {
+        std::cout << "vec[" << i << "]: " << vec[i] << std::endl;
+    }
+}
+
 int main() {
     Vectors arr(3);
-    std::cout << arr[0] << " " << arr.size() << std::endl;
+    for (int i = 0; i < arr.size(); i++) {
+        arr[i] = i;
+    }
+    arr.resize(5);
+    std::cout << "--------------------" << std::endl;
+    printVector(arr);
+    std::cout << "--------------------" << std::endl;
+
+    arr.resize(2);
+    printVector(arr);
+    std::cout << "--------------------" << std::endl;
+
+    Vectors bar = arr;
+    bar[0] = 10000;
+    printVector(bar);
+    std::cout << "--------------------" << std::endl;
+    arr.resize(0);
     return 0;
 }
