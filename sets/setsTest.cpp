5db1fef4c7f0fc64e0a28416aaff13cd429b02bf
chriscco <2444628489c@gmail.com>
2024-10-10 15:03:41 -0700

    demonstration

--- sets/setsTest.cpp
+++ sets/setsTest.cpp
@@ -7,6 +7,10 @@ int main() {
     Sets s;
     s.insert(1);
     s.insert(2);
-    std::cout << s.insert(3) << std::endl;
-    std::cout << ;
+    s.insert(3);
+    s.insert(0);
+    s.insert(18);
+    s.insert(30);
+    std::cout << s.find(3) << std::endl;
+    return 0;
 }
