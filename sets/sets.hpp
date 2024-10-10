c7ecbfe82d3545e287c4c72e0aebac44b63683ad
chriscco <2444628489c@gmail.com>
2024-10-10 12:47:22 -0700

    find

--- sets/sets.hpp
+++ sets/sets.hpp
@@ -1,1 +1,26 @@
 #pragma once
+struct Node {
+    Node *parent;
+    Node* left;
+    Node* right;
+    int val;
+};
+
+class Sets {
+    Node* root = nullptr;
+
+    Node* find(int val) {
+        Node* curr = root;
+        while (curr != nullptr) {
+            if (curr->val < val) {
+                curr = curr->left;
+                continue;
+            } else if (curr->val > val) {
+                curr = curr->left;
+                continue;
+            }
+            break;
+        }
+        return curr;
+    }
+};
