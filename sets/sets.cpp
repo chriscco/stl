5a9ba8fe604e55c52b8828431bde5691d5384a48
chriscco <2444628489c@gmail.com>
2024-10-10 14:47:04 -0700

    renamed

--- sets/sets.cpp
+++
@@ -1,1 +0,0 @@
-#include "sets.hpp"
--- sets/sets.hpp
+++ sets/sets.hpp
@@ -8,7 +8,7 @@ struct Node {
 
 class Sets {
     Node* root = nullptr;
-
+public:
     Node* find(int val) {
         Node* curr = root;
         while (curr != nullptr) {
@@ -22,5 +22,27 @@ class Sets {
             break;
         }
         return curr;
+    }
+
+    bool insert(int val) {
+        Node* node = new Node;
+        node->val = val;
+        node->right = nullptr;
+        node->left = nullptr;
+
+        Node** parent = &root;
+        while (*parent != nullptr) {
+            if ((*parent)->val < val) {
+                parent = &(*parent)->right;
+                continue;
+            } else if ((*parent)->val > val) {
+                parent  = &(*parent)->left;
+                continue;
+            }
+            return false; // 找到了相同值的节点
+        }
+        *parent = node;
+
+        return true;
     }
 };
