12d2271c10d1b5c7511cf42743da418b71df6fd5
chriscco <2444628489c@gmail.com>
2024-10-10 15:03:59 -0700

    insert

--- sets/sets.hpp
+++ sets/sets.hpp
@@ -13,7 +13,7 @@ public:
         Node* curr = root;
         while (curr != nullptr) {
             if (curr->val < val) {
-                curr = curr->left;
+                curr = curr->right;
                 continue;
             } else if (curr->val > val) {
                 curr = curr->left;
@@ -30,18 +30,21 @@ public:
         node->right = nullptr;
         node->left = nullptr;
 
-        Node** parent = &root;
-        while (*parent != nullptr) {
-            if ((*parent)->val < val) {
-                parent = &(*parent)->right;
+        Node** p_next = &root;
+        Node* parent = nullptr;
+        while (*p_next != nullptr) {
+            parent = *p_next;
+            if (parent->val < val) {
+                p_next = &parent->right;
                 continue;
-            } else if ((*parent)->val > val) {
-                parent  = &(*parent)->left;
+            } else if (parent->val > val) {
+                p_next  = &parent->left;
                 continue;
             }
             return false; // 找到了相同值的节点
         }
-        *parent = node;
+        node->parent = parent;
+        *p_next = node;
 
         return true;
     }
