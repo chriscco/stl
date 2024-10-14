2479714333e44837447e66b7cb23c8585e61ea28
chriscco <2444628489c@gmail.com>
2024-10-13 21:06:38 -0700

    updated for impl

--- sets/sets.hpp
+++ sets/sets.hpp
@@ -2,129 +2,33 @@
 #include "utils/tree.hpp"
 
 template <class T, class compare = std::less<T>, class allocator = std::allocator<T>>
-class Sets {
-    TreeNode* root = nullptr;
+class Sets : protected TreeBase<T> {
+private:
+    using iterator = typename TreeBase<T>::iterator;
+    using const_iterator = typename TreeBase<T>::const_iterator;
 public:
-    TreeNode* find(int val) {
-        TreeNode* curr = root;
-        while (curr != nullptr) {
-            if (curr->val < val) {
-                curr = curr->right;
-                continue;
-            } else if (curr->val > val) {
-                curr = curr->left;
-                continue;
-            }
-            break;
-        }
-        return curr;
+    Sets() = default;
+
+    std::pair<iterator, bool> insert(int val) {
+        return this->single_insert(val);
     }
 
-    void rotate_right(TreeNode* node) {
-        TreeNode *left = node->left;
-        node->left = left->right;
-        if (left->right != nullptr) {
-            left->right->parent = node;
-        }
-        left->parent = node->parent;
-        if (node->parent == nullptr) {
-            root = left;
-        } else if (node == node->parent->left) {
-            node->parent->left = left;
-        } else {
-            node->parent->right = left;
-        }
-        left->right = node;
-        node->parent = left;
+    iterator find(int val) {
+        return this->_M_find(val);
     }
 
-    void rotate_left(TreeNode* node) {
-        TreeNode *right = node->right;
-        node->right = right->left;
-        if (right->left != nullptr) {
-            right->left->parent = node;
-        }
-        right->parent = node->parent;
-        if (node->parent == nullptr) {
-            root = right;
-        } else if (node == node->parent->right) {
-            node->parent->right = right;
-        } else {
-            node->parent->left = right;
-        }
-        right->left = node;
-        node->parent = right;
+    const_iterator find(int val) const noexcept {
+        return this->_M_find(val);
     }
 
-    void fix_violation(TreeNode* node) {
-        while (true) {
-            TreeNode* parent = node->parent;
-            if (parent == nullptr) {
-                node->color = BLACK;
-                return;
-            }
-            if (node->color == RED || parent->color == RED) return;
+};
 
-            TreeNode *uncle, *grandpa = parent->parent;
-
-            Direction parent_direction = parent == grandpa->left ? LEFT : RIGHT;
-            if (parent_direction == LEFT) {
-                uncle = grandpa->right;
-            } else uncle = grandpa->left;
-
-            Direction node_direction = node == parent->left ? LEFT : RIGHT;
-            if (uncle->color == RED) {
-                // 1. uncle是红色节点
-                uncle->color = BLACK;
-                parent->color = BLACK;
-                grandpa->color = RED;
-                fix_violation(grandpa);
-            } else {
-                if (parent_direction == LEFT && node_direction == LEFT) {
-                    // 2. uncle是黑色节点 && parent和node在同侧(LL)
-                    rotate_right(grandpa);
-                    std::swap(parent->color, grandpa->color);
-                    if (grandpa->color == RED) fix_violation(grandpa);
-                } else if (parent_direction == RIGHT && node_direction == RIGHT) {
-                    // 2. uncle是黑色节点 && parent和node在同侧(RR)
-                    rotate_left(grandpa);
-                    std::swap(parent->color, grandpa->color);
-                    if (grandpa->color == RED) fix_violation(grandpa);
-                } else if (parent_direction == LEFT && node_direction == RIGHT) {
-                    // 2. uncle是黑色节点 && parent和node在不同侧(LR)
-                    rotate_right(parent);
-                } else if (parent_direction == RIGHT && node_direction == LEFT) {
-                    // 2. uncle是黑色节点 && parent和node在不同侧(RL)
-                    rotate_left(parent);
-                }
-            }
-        }
-    }
-
-    std::pair<TreeNode*, bool> insert(int val) {
-        TreeNode** p_next = &root;
-        TreeNode* parent = nullptr;
-        while (*p_next != nullptr) {
-            parent = *p_next;
-            if (parent->val < val) {
-                p_next = &parent->right;
-                continue;
-            } else if (parent->val > val) {
-                p_next  = &parent->left;
-                continue;
-            }
-            return {parent, false}; // 找到了相同值的节点
-        }
-        auto* node = new TreeNode;
-        node->val = val;
-        node->right = nullptr;
-        node->left = nullptr;
-        node->color = RED;
-
-        node->parent = parent;
-        *p_next = node;
-        fix_violation(node);
-
-        return {node, true};
+template <class T, class compare = std::less<T>, class allocator = std::allocator<T>>
+class MultiSet : TreeBase<T> {
+private:
+    using iterator = typename TreeBase<T>::iterator;
+public:
+    iterator insert(int val) {
+        return this->multi_insert(val);
     }
 };
