937c2c507ff26ba3784457c6ef6e2b39aa2b9277
chriscco <2444628489c@gmail.com>
2024-10-15 15:04:07 -0700

    removed all

--- sets/setsTest.cpp
+++ sets/setsTest.cpp
@@ -3,15 +3,4 @@
 #include "sets.hpp"
 
 
-int main() {
-    Sets s;
-    s.insert(1);
-    s.insert(2);
-    s.insert(3);
-    s.insert(0);
-    s.insert(18);
-    s.insert(30);
-    std::cout << s.insert(39).first->val << std::endl;
-    std::cout << s.find(3) << std::endl;
-    return 0;
-}
+int main() {}
